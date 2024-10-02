fedc50fbc0f1b376f14bc82346065e9fe77e2c63
nicolinanda <159554736+nicolinanda@users.noreply.github.com>
2024-10-02 07:45:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Qual minha cor favorita?',
+    'Verde'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do brasil?',
+    'A capital do brasil é brasilia'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz obrigado em Inglês?',
+    'obrigado em ingles é THANCS'
+)
